f13d829ec2ad18f878d52872eb517b14cad8bd0a
psychehose <psychehose@gmail.com>
2023-05-16 14:28:22 +0900

    Test: Unit Test contract code using sha256

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -31,11 +31,13 @@ impl Contract {
     }
 
     // 로그에 일정량의 가스가 필요하기 떄문에
-    pub fn guess_solution(&mut self, solution: String) {
+    pub fn guess_solution(&mut self, solution: String) -> bool{
         if solution == self.crossword_solution {
-            env::log_str("You guessed right")
+            env::log_str("You guessed right");
+            return true;
         } else {
-            env::log_str(("Try again"))
+            env::log_str(("Try again"));
+            return  false;
         }
     }
 }
@@ -47,24 +49,55 @@ impl Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use near_sdk::test_utils::{get_logs, VMContextBuilder};
+    use near_sdk::{testing_env, AccountId};
 
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+    fn get_context(predecessor: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
+    }
+
+    fn get_hash(string: &String) -> String {
+        let hash_bytes = env::sha256(string.as_bytes());
+        let hash_string = hex::encode(hash_bytes);
+        hash_string
     }
 
     #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
+    fn check_guess_solution() {
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        let context = get_context(alice);
+        testing_env!(context.build());
+
+        let mut contract = Contract {
+            crossword_solution: "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string()
+        };
+        let mut guess_result = contract.guess_solution("It is Wrong Answer".to_string());
+        assert!(!guess_result, "Expected a failure from the wrong guess");
+
+        guess_result = contract.guess_solution(get_hash(&"near nomicon ref finance".to_string()));
+        assert!(guess_result, "Expected the correct answer to return true.");
+
         assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
+            get_logs(),
+            ["Try again", "You guessed right"],
+            "Expected a successful log after the previous failed log."
         );
+
+    }
+
+    #[test]
+    fn debug_get_hash() {
+        // Basic set up for a unit test
+        // contract 단위 테스트를 위한 set up!
+        testing_env!(VMContextBuilder::new().build());
+
+        let debug_solution = "near nomicon ref finance";
+        let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
+        let debug_hash_string = hex::encode(debug_hash_bytes);
+        println!("Let's debug: {:?}", debug_hash_string);
+
+
     }
 }

9cf96c514cb8e443fccc139202b084ef02705023
psychehose <psychehose@gmail.com>
2023-05-16 23:57:33 +0900

    [FIX] Deploy.sh 수정 - 배포하면서, new 함수 사용

--- contract/deploy.sh
+++ contract/deploy.sh
@@ -11,3 +11,7 @@ echo ">> Deploying contract"
 
 # https://docs.near.org/tools/near-cli#near-dev-deploy
 near deploy crossword.psychehose.testnet --wasmFile ./target/wasm32-unknown-unknown/release/crossword_puzzle.wasm
+
+near deploy crossword.psychehose.testnet --wasmFile ./target/wasm32-unknown-unknown/release/crossword_puzzle.wasm \
+  --initFunction 'new' \
+  --initArgs '{"solution": "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f"}'
